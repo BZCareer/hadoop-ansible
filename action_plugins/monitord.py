e1a033544d332c191f0c6be9ff4a1542342fe4e7
Zak Hassan <zak.hassan1010@gmail.com>
2016-07-01 19:56:45 -0400

    Removing unused files from distro

--- action_plugins/monitord.py
+++
@@ -1,4 +0,0 @@
-try:
-    from ansible.plugins.action.normal import ActionModule
-except ImportError:
-    from ansible.runner.action_plugins.normal import ActionModule
--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Author: Zak Hassan
-
-export ANSIBLE_HOST_KEY_CHECKING=False
-# sudo yum update -y
-# sudo yum group install "Development Tools" -y
-# sudo yum install epel-release -y
-# sudo yum install ansible -y
